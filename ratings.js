c61eb0e291fd323e61eca7d88048618c79383701
Shankar Sridhar <shsridhar@linkedin.com>
2020-10-16 15:13:07 -0700

    checkpoint-2

--- ratings.js
+++ ratings.js
@@ -2,13 +2,13 @@ const puppeteer = require('puppeteer');
 const { writeJson } = require('fs-extra');
 
 async function getAmazonMovies(page) {
-  await page.goto('https://www.amazon.com/gp/video/storefront?node=2858905011/');
+  await page.goto('https://www.amazon.com/gp/video/storefront/');
   const amazonMovies = await page.evaluate(() => {
     const loadMoreInCategory = document.querySelectorAll('button[aria-label="Right"]');
     const buttonCount = document.querySelectorAll('button[aria-label="Right"]').length;
     for (let index = 0; index < buttonCount; index++) {
       // TODO: find a better way instead of hardcoding to 3
-      for (let clickCounter = 1; clickCounter <= 4; clickCounter++) {
+      for (let clickCounter = 1; clickCounter <= 5; clickCounter++) {
         loadMoreInCategory[index].click();
       }
     }
@@ -22,16 +22,30 @@ async function getAmazonMovies(page) {
   return amazonMovies;
 }
 
-async function getRatingByMovieName(page, movieName) {
+async function getImdbInfoMovieName(page, movieName) {
   const movieNameEncoded = movieName.replace(/\s{1,}/g, '+');
+
   await page.goto(`https://www.imdb.com/search/title/?title=${movieNameEncoded}`);
-  const op = await page.evaluate(() => {
+  const imdbInfo = await page.evaluate(() => {
+    // const resultItemNode = document.querySelector('.lister-item');
+    const linkNode = document.querySelector('.lister-item-image a[href*="title/"]');
+    const thumbnailNode = document.querySelector('.lister-item-image a[href*="title/"] img');
     const ratingNode = document.querySelector('.ratings-bar [data-value]');
+
     if (ratingNode) {
-      return ratingNode.getAttribute('data-value');
+      return {
+        rating: ratingNode.getAttribute('data-value'),
+        imdbLink: `https://imdb.com${linkNode.getAttribute('href')}`.replace(
+          /\/\?ref_=\S{1,}/g,
+          ''
+        ),
+        imdbThumbnail: thumbnailNode.getAttribute('src').replace(/\/ref=\S{1,}/g, ''),
+      };
+    } else {
+      return {};
     }
   });
-  return op;
+  return imdbInfo;
 }
 
 const getMovies = async () => {
@@ -44,9 +58,19 @@ const getMovies = async () => {
 
   const highlyRatedMovies = [];
   for (const movie of amazonMovies) {
-    const rating = parseFloat(await getRatingByMovieName(page, movie.name), 10);
-    if (typeof rating === 'number' && rating >= 5.5) {
-      highlyRatedMovies.push({ name: movie.name, rating, link: movie.link });
+    const aa = await getImdbInfoMovieName(page, movie.name);
+
+    const { rating, imdbLink, imdbThumbnail } = aa;
+    console.log(aa);
+    const numRating = parseFloat(rating, 10);
+    if (!isNaN(numRating) && numRating >= 5.5) {
+      highlyRatedMovies.push({
+        name: movie.name,
+        rating: numRating,
+        link: movie.link,
+        imdbLink,
+        imdbThumbnail,
+      });
     }
   }
 
