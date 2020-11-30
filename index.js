cba152e35cecbd0162a66afed3042764e2430d52
Shankar Sridhar <shsridhar@linkedin.com>
2020-11-29 22:01:01 -0800

    Adding support for CORS

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
+
+app.use(
+  cors({
+    methods: 'GET',
+  })
+);
 
 app.get('/imdb-ratings-amazon', async function (req, res) {
   const movieList = require('./imdb-ratings-amazon.json');
   res.send(movieList);
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 4000);
